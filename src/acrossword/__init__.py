2efa30edee4793f1e61702ce544ffc02c7dba78f
fastelectronicvegetable <xvow585@aucklanduni.ac.nz>
2022-02-25 12:41:33 +1300

    fixed init error

--- src/acrossword/__init__.py
+++ src/acrossword/__init__.py
@@ -1,4 +1,4 @@
-from documents.documents import Document, DocumentCollection
-from rankers.rank import Ranker
-from rankers.server import run
-from rankers.classifier import Classifier, Category
+from .documents.documents import Document, DocumentCollection
+from .rankers.rank import Ranker
+from .rankers.server import run
+from .rankers.classifier import Classifier, Category
